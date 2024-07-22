1f3aeb52d71419ea3fd2c16fc530c6f2e1974c73
Benign X <1341398182@qq.com>
2024-07-22 18:20:55 +0800

    update more
    Signed-off-by: Benign X <1341398182@qq.com>

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,23 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut rtn = vec![];
+        for (string, cmd) in input {
+            match cmd {
+                Command::Uppercase => rtn.push(string.to_uppercase()),
+                Command::Trim => rtn.push(string.trim().to_string()),
+                Command::Append(n) => {
+                    let mut s = string.clone();
+                    for _ in 0..n {
+                        s.push_str("bar");
+                    }
+                    rtn.push(s);
+                },
+            }
+        }
+        rtn
+    }
 }
 
 fn main() {
@@ -36,8 +52,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use crate::my_module::transformer;
     use super::Command;
 
     #[test]
